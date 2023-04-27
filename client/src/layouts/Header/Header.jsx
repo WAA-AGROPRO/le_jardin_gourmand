8314d36a57b5741786f9d6eaf01288000ea99d7a
Gasyh3 <kevrakt@gmail.com>
2023-04-27 11:23:23 +0200

    Update header

--- client/src/layouts/Header/Header.jsx
+++ client/src/layouts/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState } from 'react'
 
 import Menu from '../../pages/Menu/Menu'
 
@@ -10,38 +10,44 @@ import TikTokLogo from '../../components/Logo/TikTok'
 import './Header.scss'
 
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   
 
-
   return (
-      <header className="header">
-        <Link to={"/"} className="header__logo">Le Jardin Gourmand</Link>
-        <nav className="header__nav">
-          <ul className="header__nav__list">
-            <li className="header__nav__list__item">
-              <a href="https://www.thefork.fr/restaurant/le-jardin-gourmand-r1722" className="header__nav__list__item__link">Réservation</a>
+    <nav className={`nav ${isMenuOpen ? "show" : "hide"}`}>
+        <Link to={"/"} className="nav__logo">Le Jardin Gourmand</Link>
+          <ul className="nav__links">
+            <li className="nav__item">
+              <a href="https://www.thefork.fr/restaurant/le-jardin-gourmand-r1722" className="nav__link">Réservation</a>
             </li>
-            <li className="header__nav__list__item">
-              <a href="https://lejardin-gourmand.byclickeat.fr/order" className="header__nav__list__item__link">Livraison</a>
+            <li className="nav__item">
+              <a href="https://lejardin-gourmand.byclickeat.fr/order" className="nav__link">Livraison</a>
             </li>
-            <li className="header__nav__list__item">
-              <Link to={"menu"} className="header__nav__list__item__link">Menu</Link>
+            <li className="nav__item">
+              <Link to={"menu"} className="nav__link">Menu</Link>
             </li>
-          </ul>
-          <section className="header__social">
-          <a href="https://www.facebook.com/lejardingourmand.amberieux" target="_blank" className="header__social__link">
+            <section className="social">
+          <a href="https://www.facebook.com/lejardingourmand.amberieux" target="_blank" className="social__link">
             <FBLogo /> 
           </a>  
-          <a href="https://www.instagram.com/?hl=fr" className="header__social__link" target="_blank">
+          <a href="https://www.instagram.com/?hl=fr" className="social__link" target="_blank">
             <InstaLogo />
           </a>
-          <a href="https://www.instagram.com/?hl=fr" className="header__social__link" target="_blank">
+          <a href="https://www.instagram.com/?hl=fr" className="social__link" target="_blank">
             <TikTokLogo />
           </a>
           </section>
-        </nav>
-      </header>
-
+          </ul>
+          
+          <button className="nav__burger" onClick={toggleMenu}>
+            <span className={`burger-bar`}>
+            </span>
+          </button>
+    </nav>
     )
 }
 
